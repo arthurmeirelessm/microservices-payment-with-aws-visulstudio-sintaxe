311ac8b21119fc67e44c4710ee82f49534e4cf53
Arthur Mendes <arthur.mendes@compasso.com.br>
2023-06-12 10:10:43 -0300

    First commit

---
+++ SQS.Fornecedor/Program.cs
@@ -0,0 +1,47 @@
+﻿using Amazon.SQS;
+using Amazon;
+using Amazon.SQS.Model;
+using SQS.Fornecedor.models;
+using DotNetEnv;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+
+        var bodyMessageJSON = new BodyMessageJSON()
+        {
+            Body = "",
+            Status = "200"
+        };
+
+        DotNetEnv.Env.Load();
+
+        var messageConvert = JsonSerializer.Serialize(bodyMessageJSON);
+        var userEndpointAWS = Environment.GetEnvironmentVariable("USER_ENDPOINT_AWS");
+
+        var client = new AmazonSQSClient(RegionEndpoint.USWest2);
+
+        var request = new SendMessageRequest
+        {
+            QueueUrl = userEndpointAWS,
+            MessageBody = messageConvert
+        };
+
+        await client.SendMessageAsync(request);
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
---
+++ SQS.Fornecedor/models/MessageBodyJSON.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQS.Fornecedor.models
+{
+    public class BodyMessageJSON
+    {
+
+        public string? Body { get; set; }
+
+        public string? Status { get; set; }
+
+    }
+}
