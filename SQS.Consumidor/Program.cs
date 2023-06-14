31d3dd4757339f11ea2d29d6b8811b30a723788c
Arthur Mendes <arthur.mendes@compasso.com.br>
2023-06-14 19:44:38 -0300

    Add and testing consumer- OK

--- SQS.Consumidor/Program.cs
+++ SQS.Consumidor/Program.cs
@@ -1,1 +1,40 @@
-﻿
+﻿using Amazon.SQS;
+using Amazon;
+using Amazon.SQS.Model;
+using DotNetEnv;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using System.Reflection.Metadata.Ecma335;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        DotNetEnv.Env.Load();
+        var client = new AmazonSQSClient(RegionEndpoint.USWest2);
+        var userEndpointAWS = Environment.GetEnvironmentVariable("USER_ENDPOINT_AWS");
+
+
+        var request = new ReceiveMessageRequest
+        {
+            QueueUrl = userEndpointAWS
+        };
+
+
+        var capturedMessageResponse = await client.ReceiveMessageAsync(request);
+        foreach (var message in capturedMessageResponse.Messages)
+        {
+            await Console.Out.WriteLineAsync(message.Body);
+            await client.DeleteMessageAsync(userEndpointAWS, message.ReceiptHandle);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
